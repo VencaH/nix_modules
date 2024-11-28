90d402dbdae2a5b48150fb9de7677b5a051bf308
Venca H <venca.hyncica@gmail.com>
2024-11-28 22:40:02 +0100

    [Fix] Fixes path to the files in nix_modules.

--- common_apps.nix
+++ common_apps.nix
@@ -1,12 +1,12 @@
 { config, pkgs, ...}:
 {
   imports = [
-    ../home/nix_modules/helix.nix
-    ../home/nix_modules/alacritty.nix
-    ../home/nix_modules/zellij.nix
-    ../home/nix_modules/git.nix
-    ../home/nix_modules/starship.nix
-    ../home/nix_modules/nushell.nix
-    ../home/nix_modules/nixvim.nix
+    ./helix.nix
+    ./alacritty.nix
+    ./zellij.nix
+    ./git.nix
+    ./starship.nix
+    ./nushell.nix
+    ./nixvim.nix
   ];
 }
