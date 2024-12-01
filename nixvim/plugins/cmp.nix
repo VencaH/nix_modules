b524c807a2b0933ffa85063e8ebaabff7913c905
Venca H <venca.hyncica@gmail.com>
2024-12-01 20:15:10 +0100

    [Fix] Fixed issues with <Tab> key and navigation in autocomplete.

--- nixvim/plugins/cmp.nix
+++ nixvim/plugins/cmp.nix
@@ -1,30 +1,44 @@
 {
-  plugins.cmp = {
-    enable = true;
-    autoEnableSources = true;
-    settings = {
+      plugins.cmp = {
+      enable = true;
+      settings =  {
+      autoEnableSources = true;
       sources = [
         {name = "nvim_lsp";}
         {name = "path";}
         {name = "buffer";}
-     ];
-     mapping = {
+        {name = "luasnip";}
+      ];
+
+      mapping = {
         "<CR>" = "cmp.mapping.confirm({ select = true })";
         "<Tab>" = "cmp.mapping(
-          function(fallback)
+            function(fallback)
+              luasnip = require('luasnip')
               if cmp.visible() then
                 cmp.select_next_item()
               elseif luasnip.expandable() then
                 luasnip.expand()
               elseif luasnip.expand_or_jumpable() then
                 luasnip.expand_or_jump()
-              elseif check_backspace() then
-                fallback()
               else
                 fallback()
               end
-            end, {'i', 's' })";
-      };
-    };
-  };
+            end
+          ,{'i','s'})";
+        "<S-Tab>" = "cmp.mapping(
+            function(fallback)
+              luasnip = require('luasnip')
+              if cmp.visible() then
+                cmp.select_prev_item()
+              elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+              else
+                fallback()
+            end
+        end, {'i','s'}
+        )";
+        };
+    };# snippets
+};
 }
